4b9288cef0e9cf9a7bc742f057220329cae584c4
julien <julien.milants@gmail.com>
2023-12-13 14:43:18 +0100

    format

--- app/connected/page.tsx
+++ app/connected/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div className='pied'>
-        <h1>Tu es login mon reuf</h1>
+    <div className="pied">
+      <h1>Tu es login mon reuf</h1>
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,20 @@
-import Image from 'next/image'
-import avatar from '../src/img/login/avatar.svg'
+import Image from "next/image";
+import avatar from "../src/img/login/avatar.svg";
 import "../src/scss/App.scss";
 export default function page() {
   return (
     <>
       <div className="login_view">
         <div className="Date_Hour">
-          <p className='date'>Wednesday Dec 6</p>
-          <p className='hour'>14:32</p>
+          <p className="date">Wednesday Dec 6</p>
+          <p className="hour">14:32</p>
         </div>
         <div className="login_form">
           <div className="avatar"></div>
           <div className="input">
-          <Image
-              src={avatar}
-              width={50}
-              height={50}
-              alt="Avatar"
-            />
+            <Image src={avatar} width={50} height={50} alt="Avatar" />
             <form action="">
-              <input type="text" placeholder="Enter Password..."/>
+              <input type="text" placeholder="Enter Password..." />
             </form>
           </div>
         </div>
