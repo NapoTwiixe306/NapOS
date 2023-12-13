59c17eb78053a53f08e03687d73e8ba112e7f242
julien <julien.milants@gmail.com>
2023-12-13 16:35:51 +0100

    topbar ok

--- src/Components/Topbar.tsx
+++ src/Components/Topbar.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import icon from "../img/icons/icons.png";
+import battery from "../img/icons/Battry.svg";
+import search from '../img/icons/Search.svg'
+import wifi from '../img/icons/Wifi.svg'
 import { Topbar_List_Default } from "./Topbar_List_default";
 
 export default function Topbar() {
@@ -11,11 +14,36 @@ export default function Topbar() {
         </div>
         <div className="list">
           {Topbar_List_Default.map((item, index) => (
-            <p key={index}>
+            <div key={index}>
               {item.title && <strong>{item.title}</strong>}
-              {item.name && <span>{item.name}</span>}
-            </p>
+              {item.name && (
+                <div className="dropdown">
+                  <button className="dropbtn">{item.name}</button>
+                  <div className="dropdown-content">
+                    <p className="dropdown-options">Option 1</p>
+                    <p className="dropdown-options">Option 1</p>
+                    <p className="dropdown-options">Option 1</p>
+                    <p className="dropdown-options">Option 1</p>
+                  </div>
+                </div>
+              )}
+            </div>
           ))}
+        </div>
+        <div className="icons_topbar">
+          <Image
+            src={battery}
+            alt="Battry"
+          />
+          <Image
+            src={search}
+            alt="Battry"
+          />
+          <Image
+            src={wifi}
+            alt="Battry"
+          />
+          <p className="date">Jeu. 7 Déc. 11:24</p>
         </div>
       </div>
     </div>
