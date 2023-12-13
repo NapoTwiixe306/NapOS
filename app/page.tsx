e10f32f7251d30295dd56b4c6973414a611ed01a
julien <julien.milants@gmail.com>
2023-12-13 13:19:46 +0100

    design login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import Image from 'next/image'
+import avatar from '../src/img/login/avatar.svg'
 import "../src/scss/App.scss";
 export default function page() {
   return (
-    <div className="pied">
-      <h1>page</h1>
-    </div>
+    <>
+      <div className="login_view">
+        <div className="Date_Hour">
+          <p className='date'>Wednesday Dec 6</p>
+          <p className='hour'>14:32</p>
+        </div>
+        <div className="login_form">
+          <div className="avatar"></div>
+          <div className="input">
+          <Image
+              src={avatar}
+              width={50}
+              height={50}
+              alt="Avatar"
+            />
+            <form action="">
+              <input type="text" placeholder="Enter Password..."/>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
