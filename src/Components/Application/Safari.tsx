dc1665f185106c44943d575632760069de76d71d
julien <julien.milants@gmail.com>
2023-12-14 18:01:38 +0100

    test sur le pc portable

--- src/Components/Application/Safari.tsx
+++ src/Components/Application/Safari.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Safari() {
-  return <div>Safari</div>;
+  return (
+    <>
+      <div className="safari_container">
+        <div className="safari_topbar"></div>
+        <div className="safari_content"></div>
+      </div>
+    </>
+  );
 }
