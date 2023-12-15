721e33d213e14dd0c7d44015d96b3a1ce547e3b7
julien <julien.milants@gmail.com>
2023-12-15 20:00:55 +0100

    format

--- src/Components/Application/Safari.tsx
+++ src/Components/Application/Safari.tsx
@@ -5,7 +5,44 @@ export default function Safari() {
     <>
       <div className="safari_container">
         <div className="safari_topbar">
-          <p>1</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="52"
+            height="12"
+            viewBox="0 0 52 12"
+            fill="none"
+          >
+            <circle cx="46" cy="6" r="6" fill="#5FCF65" />
+            <circle cx="26" cy="6" r="6" fill="#F8CE52" />
+            <circle cx="6" cy="6" r="6" fill="#F96057" />
+            <circle
+              cx="6"
+              cy="6"
+              r="5.75"
+              stroke="black"
+              strokeOpacity="0.1"
+              strokeWidth="0.5"
+              style={{ mixBlendMode: "luminosity" }}
+            />
+            <circle
+              cx="26"
+              cy="6"
+              r="5.75"
+              stroke="black"
+              strokeOpacity="0.1"
+              strokeWidth="0.5"
+              style={{ mixBlendMode: "luminosity" }}
+            />
+            <circle
+              cx="46"
+              cy="6"
+              r="5.75"
+              stroke="black"
+              strokeOpacity="0.1"
+              strokeWidth="0.5"
+              style={{ mixBlendMode: "luminosity" }}
+            />
+          </svg>
         </div>
         <div className="safari_content">
           <p>lorem*500</p>
