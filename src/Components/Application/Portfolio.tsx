5aff3f88b7e32bbef47ebc602eb86d6da81f8d26
julien <julien.milants@gmail.com>
2023-12-16 18:11:36 +0100

    onclick button portfolio

--- src/Components/Application/Portfolio.tsx
+++ src/Components/Application/Portfolio.tsx
@@ -13,9 +13,12 @@ const Portfolio: React.FC<PortfolioProps> = ({ onClose }) => {
   };
 
   return (
-    <div onClick={handleClose}>
-      Portfolio
+    <>
+    <div>
+        <button onClick={handleClose}>Close</button>
+        <p>lorem*500</p>
     </div>
+    </>
   );
 };
 
