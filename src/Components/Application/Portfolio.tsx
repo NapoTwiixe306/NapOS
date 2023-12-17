8ee3262b0ece0aac4071b523bb7a356507e14e86
julien <julien.milants@gmail.com>
2023-12-17 19:48:54 +0100

    add close btn portfolio.tsx

--- src/Components/Application/Portfolio.tsx
+++ src/Components/Application/Portfolio.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import Test from "../../img/Shape.png";
 
 interface PortfolioProps {
   onClose: () => void;
 }
 
 const Portfolio: React.FC<PortfolioProps> = ({ onClose }) => {
-
+  const handleClose = () => {
+    if (onClose) {
+      onClose();
+    }
+  };
 
   return (
     <>
-      <div className="test">
+      <div className="Portfolio_container">
+      <button onClick={handleClose}>Close</button>
+        <p>lorem*500</p>
       </div>
     </>
   );
