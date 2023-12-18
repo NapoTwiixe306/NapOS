31013e99d2416f5d11fe223641e994f744e0d7c6
julien <julien.milants@gmail.com>
2023-12-18 18:36:10 +0100

    "finish" left side Settings.tsx

--- src/Components/Application/Settings.tsx
+++ src/Components/Application/Settings.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import close from "../../img/close.svg";
+import avatar from "../../img/login/avatar.svg";
+import { Settings_List } from "../List/Settings_List";
 
 export default function Settings() {
+  const [selectedTab, setSelectedTab] = useState<string | null>(null);
+  const [showNotification, setShowNotification] = useState<boolean>(true); // Mettez cette valeur à true pour afficher la notification
+
+  // Fonction pour gérer le clic sur un élément <p>
+  const handleTabClick = (tabName: string) => {
+    setSelectedTab(tabName);
+  };
+
   return (
     <>
       <div className="Settings_Container">
@@ -10,7 +20,32 @@ export default function Settings() {
           <div className="item">
             <div className="left">
               <Image src={close} alt="close" />
-              <p>left</p>
+              <div className="input">
+                <input type="search" name="" id="" placeholder="Rechercher" />
+              </div>
+              <div className="user">
+                <Image src={avatar} alt="avatar" width={50} height={50} />
+                <div className="info">
+                  <p>Users</p>
+                  <p>Identifian Apple</p>
+                </div>
+              </div>
+              <div className="maj">
+                {showNotification && <div className="notific">1</div>}
+                <p>Mise à jour logiciel disponible</p>
+              </div>
+              <div className="item">
+                {Settings_List.map((tab) => (
+                  <div key={tab.name} className={tab.name}>
+                    <p
+                      className={selectedTab === tab.name ? "selected" : ""}
+                      onClick={() => handleTabClick(tab.name)}
+                    >
+                      {tab.label}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
             <div className="sepa"></div>
             <div className="right">right</div>
---
+++ src/Components/List/Settings_List.ts
@@ -0,0 +1,12 @@
+
+export const Settings_List = [
+    { name: "Wifi", label: "Wifi" },
+    { name: "Bluetooth", label: "Bluetooth" },
+    { name: "Réseau", label: "Réseau" },
+    { name: "Notifications", label: "Notifications" },
+    { name: "Son", label: "Son" },
+    { name: "Concentration", label: "Concentration" },
+    { name: "Temps d'écran", label: "Temps d'écran" },
+    { name: "Général", label: "Général" },
+    { name: "Themes", label: "Themes" },
+];
