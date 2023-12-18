372b167d982c7c6ce1cafe37e3eaac5ea02348fa
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-12-18 10:47:36 +0100

    add right && left

--- src/Components/Application/Settings.tsx
+++ src/Components/Application/Settings.tsx
@@ -4,7 +4,12 @@ export default function Settings() {
   return (
     <>
       <div className="Settings_Container">
-        <div className="Container"></div>
+        <div className="Container">
+          <div className="item">
+            <div className="left">left</div>
+            <div className="right">right</div>
+          </div>
+        </div>
       </div>
     </>
   );
