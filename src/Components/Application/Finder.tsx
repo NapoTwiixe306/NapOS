98880804efcb43a246c20cb66915534fa9774f41
julien <julien.milants@gmail.com>
2023-12-23 15:13:15 +0100

    continue Finder.tsx

--- src/Components/Application/Finder.tsx
+++ src/Components/Application/Finder.tsx
@@ -8,7 +8,22 @@ export default function  Finder(){
                     <div className="items">
                         <div className="left">
                             <Image src={close as any} alt="close" className="close_img"/>
-
+                            <div className="Favorites">
+                                <h1>Favorites</h1>
+                                <p>Recents</p>
+                                <p>Desktop</p>
+                                <p>Documents</p>
+                                <p>Downloads</p>
+                            </div>
+                            <div className="Icloud">
+                                <h1>Icloud</h1>
+                                <p>Icloud Drive</p>
+                            </div>
+                            <div className="Locations">
+                                <h1>Locations</h1>
+                                <p>Macintosh HD</p>
+                                <p>Network</p>
+                            </div>
                         </div>
                         <div className="sepa"></div>
                         <div className="right">
