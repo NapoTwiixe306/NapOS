dd50207de18247eea35b2c3922ed408e15b76ce4
julien <julien.milants@gmail.com>
2023-12-24 11:58:20 +0100

    add tempo dark theme for wallpaper-dark.png

--- src/Components/Application/Finder.tsx
+++ src/Components/Application/Finder.tsx
@@ -2,72 +2,83 @@
 import close from "../../img/close.svg";
 import Image from "next/image";
 import { Finder_List } from "../List/Finder_List";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
 export default function Finder() {
   const [selectedTab, setSelectedTab] = useState<string | null>(null);
   const [showNotification, setShowNotification] = useState<boolean>(true);
+  const [darkMode, setDarkMode] = useState<boolean>(false);
 
   const handleTabClick = (tabName: string) => {
     setSelectedTab(tabName);
   };
-
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+  useEffect(() => {
+    // Mettez à jour la classe du body lors du changement du mode sombre
+    document.body.classList.toggle("dark", darkMode);
+  }, [darkMode]);
   return (
     <>
       <div className="finder">
         <div className="Container">
           <div className="items">
             <div className="left">
-              <Image src={close as any} alt="close" className="close_img" />
+              <Image src={close as any} alt="close" className="close_img"/>
               <div className="content">
                 <div className="Favorites">
                   <span>Favorites</span>
                   {Finder_List.filter((item) =>
-                    ["Recents", "Desktop", "Documents", "Downloads"].includes(
-                      item.name,
-                    ),
+                      ["Recents", "Desktop", "Documents", "Downloads"].includes(
+                          item.name,
+                      ),
                   ).map((item) => (
-                    <p
-                      key={item.name}
-                      className={selectedTab === item.name ? "selected" : ""}
-                      onClick={() => handleTabClick(item.name)}
-                    >
-                      {item.name}
-                    </p>
+                      <p
+                          key={item.name}
+                          className={selectedTab === item.name ? "selected" : ""}
+                          onClick={() => handleTabClick(item.name)}
+                      >
+                        {item.name}
+                      </p>
                   ))}
                 </div>
                 <div className="Icloud">
                   <span>Icloud</span>
                   {Finder_List.filter(
-                    (item) => item.name === "Icloud Drive",
+                      (item) => item.name === "Icloud Drive",
                   ).map((item) => (
-                    <p
-                      key={item.name}
-                      className={selectedTab === item.name ? "selected" : ""}
-                      onClick={() => handleTabClick(item.name)}
-                    >
-                      {item.name}
-                    </p>
+                      <p
+                          key={item.name}
+                          className={selectedTab === item.name ? "selected" : ""}
+                          onClick={() => handleTabClick(item.name)}
+                      >
+                        {item.name}
+                      </p>
                   ))}
                 </div>
                 <div className="Locations">
                   <span>Locations</span>
                   {Finder_List.filter((item) =>
-                    ["Macintosh HD", "Network"].includes(item.name),
+                      ["Macintosh HD", "Network"].includes(item.name),
                   ).map((item) => (
-                    <p
-                      key={item.name}
-                      className={selectedTab === item.name ? "selected" : ""}
-                      onClick={() => handleTabClick(item.name)}
-                    >
-                      {item.name}
-                    </p>
+                      <p
+                          key={item.name}
+                          className={selectedTab === item.name ? "selected" : ""}
+                          onClick={() => handleTabClick(item.name)}
+                      >
+                        {item.name}
+                      </p>
                   ))}
                 </div>
               </div>
             </div>
             <div className="sepa"></div>
-            <div className="right">right</div>
+            <div className={`right ${darkMode ? "dark" : ""}`}>
+              <button onClick={toggleDarkMode}>
+                {darkMode ? "Disable Dark Mode" : "Enable Dark Mode"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
