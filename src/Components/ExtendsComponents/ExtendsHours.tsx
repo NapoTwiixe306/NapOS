d395c87919f56ee6eeb76df06b441aaf7b39f053
julien <julien.milants@gmail.com>
2024-01-08 18:07:03 +0100

    init extends hour

--- src/Components/ExtendsComponents/ExtendsHours.tsx
+++ src/Components/ExtendsComponents/ExtendsHours.tsx
@@ -3,17 +3,17 @@ export default function Extends() {
     <div className="Extends_Container">
       <div className="content">
         <div className="top">
-          <div className="network"></div>
-          <div className="concentration"></div>
-          <div className="stageManager"></div>
-          <div className="copyScreen"></div>
+          <div className="network">1</div>
+          <div className="concentration">2</div>
+          <div className="stageManager">3</div>
+          <div className="copyScreen">4</div>
         </div>
         <div className="middle">
-          <div className="moniteur"></div>
-          <div className="Son"></div>
+          <div className="moniteur">1</div>
+          <div className="Son">2</div>
         </div>
         <div className="bottom">
-          <div className="batterie"></div>
+          <div className="batterie">1</div>
         </div>
       </div>
     </div>
