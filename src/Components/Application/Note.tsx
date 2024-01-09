d907f74c9846492ec717ad5945620bcfacd8ed08
julien <julien.milants@gmail.com>
2024-01-09 17:27:28 +0100

    add title note app

--- src/Components/Application/Note.tsx
+++ src/Components/Application/Note.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Note() {
   return (
     <div className="Note_Container">
-      <div className="Container"></div>
+      <div className="Container">
+        <h1>Note</h1>
+      </div>
     </div>
   );
 }
